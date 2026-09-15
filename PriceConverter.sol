afe90b952a9cc235fe0d0a25430a91bd1f0eb585
Juanma <juancitobravoc@gmail.com>
2026-09-15 11:27:34 -0300

    Implement PriceConverter library for ETH to USD conversion

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.34;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+library PriceConverter {
+
+
+    function getPrice() internal view returns (uint256) {
+        (, int256 answer, , , ) = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() internal view returns(uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+}
